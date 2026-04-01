e78e71e0b7b17bbf61c12f744ed487b7f98d81dc
emptyxz <129485208+empt1xz@users.noreply.github.com>
2026-04-01 00:02:31 -0300

    fix: Removendo SectionCards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Header from "../components/Header/Header";
 import Hero from "../components/HeroComponent/Hero";
-import Cards from "../components/SectionCards/Cards";
 export default function Home() {
   return (
     <>
