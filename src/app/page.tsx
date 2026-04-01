7cc12ea188f5e130cf8e27f6a0adfc26aa50f3f4
emptyxz <129485208+empt1xz@users.noreply.github.com>
2026-04-01 19:24:20 -0300

    fix: Header/Hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Header from "../components/Header/Header";
 import Hero from "../components/HeroComponent/Hero";
+import Footer from '../components/Footer/Footer'
 export default function Home() {
   return (
     <>
     <Header/>
     <Hero/>
+    <Footer/>
     </>
   );
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,47 @@
+import Style from "./footer.module.css";
+export default function Footer() {
+  return (
+    <>
+      <footer className={Style.footer}>
+        <div className={Style.aboutus}>
+          <h1 className={Style.h1_aboutus}>12sln</h1>
+          <p className={Style.p_aboutus}>
+            12sln é uma startup de tecnologia fundada por 12 desenvolvedores
+            profissionais que vão te guiar para a soluçao do seu problema.
+          </p>
+        </div>
+        <div className={Style.navigation}>
+          <h1>Navegação</h1>
+          <ul>
+            <li>Features</li>
+            <li>Documentação</li>
+            <li>Sobre</li>
+          </ul>
+        </div>
+        <div className={Style.support}>
+          <h1>Suporte</h1>
+          <ul>
+            <li>suporte@12sln.com.br</li>
+            <li>Termos de Serviço</li>
+            <li>Política de Privacidade</li>
+          </ul>
+        </div>
+        <div className={Style.faq}>
+          <h1>Faq Rápido</h1>
+          <ul>
+            <li>Quanto tempo de prazo?</li>
+            <li>Quais são as formas de pagamento?</li>
+            <li>Existe custo de manutenção?</li>
+            <li>Vocês oferecem suporte técnico?</li>
+            <li>Vocês cuidam do domínio e hospedagem?</li>
+            <li>Posso solicitar novas funcionalidades depois?</li>
+          </ul>
+        </div>
+      </footer>
+
+      <div className={Style.copyright}>
+        <p>&copy; 2026 12sln · Todos os direitos reservados.</p>
+      </div>
+    </>
+  );
+}
