847211baf8a8da98645057c98806f79745a6edaa
emptyxz <129485208+empt1xz@users.noreply.github.com>
2026-04-03 16:46:34 -0300

    Correção de Branchs

--- src/components/Timeline/Timeline.tsx
+++ src/components/Timeline/Timeline.tsx
@@ -3,9 +3,32 @@ import Style from "./timeline.module.css";
 export default function Timeline() {
   return (
     <>
-     <main className={Style.main}>
-         <h1>Timeline</h1>
-     </main>
+      <main className={Style.main}>
+        <section className={`${Style.Titulo}`}>
+          <h1>Vossa Timeline</h1>
+          <p>santificado seja vossa linha</p>
+        </section>
+
+        <section className={`${Style.Timeline}`}>
+          <section className={`${Style.CardsLeft}`}>
+            <section className={`${Style.card}`}>
+              <h1>Card1</h1>
+            </section>
+          </section>
+
+          <div className={Style.Line}></div>
+
+          <section className={`${Style.CardsRight}`}>
+            <section className={`${Style.card}`}>
+              <h1>card1</h1>
+            </section>
+
+            <section className={`${Style.card}`}>
+              <h1>card2</h1>
+            </section>
+          </section>
+        </section>
+      </main>
     </>
   );
 }
