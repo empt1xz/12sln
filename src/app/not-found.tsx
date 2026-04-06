5fada46253e47b4ae26904fc9cb664c43ecf041b
emptyxz <129485208+empt1xz@users.noreply.github.com>
2026-04-06 18:02:29 -0300

    Not Found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,11 +4,14 @@ export default function notFound() {
   return (
     <>
       <main className="notfound">
-        <h1>Ops, essa página não existe.</h1>
+       <img src="https://i.pinimg.com/originals/67/5d/aa/675daa5c96e23bb7e2b5b06ef804cb81.gif" alt="" />
 
-        <button>
-          <Link href="/">Voltar para home</Link>
-        </button>
+          <Link href="/">Não tem nada aqui..</Link>
+          <button>
+            <Link href='/'>
+              Voltar para Home
+            </Link>
+          </button>
       </main>
     </>
   );
