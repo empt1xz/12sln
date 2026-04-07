5d748ca5585fec1cd509b8f51c217078edc42a26
emptyxz <129485208+empt1xz@users.noreply.github.com>
2026-04-07 01:48:29 -0300

    Atualização no Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,44 +1,15 @@
-"use client"
-
-
-import Style from "./footer.module.css"
-import { CircleArrowUp } from "lucide-react"
-export default function Footer() {
-
-    function handleClick(text: string) {
-        alert(text)
-    }
-
-    return (
+import Style from './footer.module.css'
+export default function Footer () {
+     return (
         <>
-
-            <footer className={Style.footer}>
-                
-                <div className={Style.aboutus}>
-                    <h1 className={Style.h1_aboutus}>12sln</h1>
-                    <p className={Style.p_aboutus}>12sln é uma startup de tecnologia fundada por 12 desenvolvedores profissionais que vão te guiar para a solução do seu problema.</p>
-                </div>
-                <div className={Style.navigation}>
-                    <h1>Navegação</h1>
-                    <ul>
-                        <li onClick={() => handleClick('a')}>Features</li>
-                        <li onClick={() => handleClick('a')}>Documentação</li>
-                        <li onClick={() => handleClick('a')}>Sobre</li>
-                    </ul>
-                </div>
-                <div className={Style.backtostart}>
-                    <a href="#hero">
-                        <CircleArrowUp size={50} />
-                    </a>
-                    <h1 className={Style.h1_backto}>Voltar ao topo</h1>
-                </div>
-
-
-            </footer>
-
-            <div className={Style.copyright}>
-                <p>&copy; 2026 12sln · Todos os direitos reservados.</p>
-            </div>
+        <footer className={Style.footer}>
+             <img src="./logotipo.png" alt="" />
+             <p>
+                <code>
+                    &copy; 2026 | Todos os direitos reservados
+                </code>
+             </p>
+        </footer>
         </>
-    )
+     )
 }
